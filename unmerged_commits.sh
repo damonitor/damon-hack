83c2ff1902bb61e52335ab7a89c525c6f5c6ea32
SeongJae Park <sj@kernel.org>
2022-12-02 00:53:12 +0000

    unmerged_commits: Support merged commits printing
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- unmerged_commits.sh
+++ unmerged_commits.sh
@@ -1,19 +1,57 @@
 #!/bin/bash
 
-if [ $# -ne 2 ] && [ $# -ne 3 ]
-then
-	echo "Usage: $0 <dev commits> <maintainer tree commits> [--human_readable]"
-	exit 1
-fi
+pr_usage()
+{
+	echo "
+Usage: $0 [OPTION] <dev commits> <maintainer tree commits>
 
-dev_commits_range=$1
-maintainer_commits_range=$2
+OPTION
+  --human_readable	Print in human readable manner
+  --merged		Print merged commits
+"
+}
 
-if [ $# -eq 3 ] && [ "$3" = "--human_readable" ]
+pr_usage_exit()
+{
+	exit_msg=$1
+	exit_code=$2
+	echo "$exit_msg"
+	echo
+	pr_usage
+	exit "$exit_code"
+}
+
+human_readable="false"
+pr_merged="false"
+
+while [ $# -ne 0 ]
+do
+	case $1 in
+	"--human_readable")
+		human_readable="true"
+		shift 1
+		continue
+		;;
+	"--merged")
+		pr_merged="true"
+		shift 1
+		continue
+		;;
+	*)
+		if [ $# -ne 2 ]
+		then
+			pr_usage_exit "wrong number of arguments" 1
+		fi
+		dev_commits_range=$1
+		maintainer_commits_range=$2
+		shift 2
+		;;
+	esac
+done
+
+if [ "$dev_commits_range" = "" ]
 then
-	human_readable="true"
-else
-	human_readable="false"
+	pr_usage_exit "" 1
 fi
 
 dev_commits=$(git log "$dev_commits_range" --pretty=%H --reverse)
@@ -25,6 +63,13 @@ do
 	dev_subject=$(git show "$dev_commit" --pretty=%s --quiet)
 
 	if echo "$maintainer_subjects" | grep "$dev_subject" --quiet --ignore-case
+	then
+		merged="true"
+	else
+		merged="false"
+	fi
+
+	if [ ! "$merged" = "$pr_merged" ]
 	then
 		continue
 	fi
