4d3bd78cedefd343785ffb4538039fdf2190e4b5
SeongJae Park <sj@kernel.org>
2022-12-22 20:47:47 +0000

    _stat_damon_lines_portion: Do not print merges included numbers
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- _stat_damon_lines_portion.sh
+++ _stat_damon_lines_portion.sh
@@ -12,12 +12,6 @@ bindir=$(dirname "$0")
 range=$1
 parent_dir=$2
 
-lines_total=$("$bindir/_stat_lines.sh" "$range" "$parent_dir" | \
-	awk '{print $3}')
-lines_damon=$("$bindir/_stat_lines.sh" "$range" mm/damon/ | awk '{print $3}')
-portion_permil=$((lines_damon * 1000 / lines_total))
-echo "$lines_damon/$lines_total ($portion_permil permil) including merges"
-
 lines_total=$("$bindir/_stat_lines.sh" "$range" "$parent_dir" --no-merges | \
 	awk '{print $3}')
 lines_damon=$("$bindir/_stat_lines.sh" "$range" mm/damon/ --no-merges | \
