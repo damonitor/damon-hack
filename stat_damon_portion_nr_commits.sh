e73b6eacac30f3f50b9e3e27cae59387d6046713
SeongJae Park <sj@kernel.org>
2022-12-22 20:05:01 +0000

    stat_damon_portion_nr_commits: Make output brief
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- stat_damon_portion_nr_commits.sh
+++ stat_damon_portion_nr_commits.sh
@@ -7,20 +7,13 @@ for version in v5.15 v5.16 v5.17 v5.18 v5.19 v6.0 v6.1 linus/master
 do
 	range="$prev_version..$version"
 	echo "# $range"
-	echo "in mm"
-	"$bindir/_stat_damon_nr_commits_portion.sh" "$range" mm
-	echo
-	echo "in linux"
-	"$bindir/_stat_damon_nr_commits_portion.sh" "$range" ./
+	echo "$("$bindir/_stat_damon_nr_commits_portion.sh" "$range" mm) # mm"
+	echo "$("$bindir/_stat_damon_nr_commits_portion.sh" "$range" ./) # linux"
 	echo
 
 	prev_version=$version
 done
 range="v5.14..linus/master"
 echo "# $range"
-echo "in mm"
-"$bindir/_stat_damon_nr_commits_portion.sh" "$range" mm
-echo
-echo "in linux"
-"$bindir/_stat_damon_nr_commits_portion.sh" "$range" ./
-echo
+echo "$("$bindir/_stat_damon_nr_commits_portion.sh" "$range" mm) # mm"
+echo "$("$bindir/_stat_damon_nr_commits_portion.sh" "$range" ./) # linux"
