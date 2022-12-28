83279bf6e3b3519d173d218eeae98c41bb6ccf47
SeongJae Park <sj@kernel.org>
2022-12-28 01:23:33 +0000

    stat_damon_portion_nr_commits: Update for better output format
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- stat_damon_portion_nr_commits.sh
+++ stat_damon_portion_nr_commits.sh
@@ -12,17 +12,36 @@ cd "$linux_dir"
 
 versions=( $(cat "$bindir/stat_branches") )
 prev_version="${versions[0]}"
+echo "range	damon	mm	damon/mm	linux	damon/linux"
 for version in "${versions[@]:1}"
 do
 	range="$prev_version..$version"
-	echo "# $range"
-	echo "$("$bindir/_stat_damon_nr_commits_portion.sh" "$range" mm) # mm"
-	echo "$("$bindir/_stat_damon_nr_commits_portion.sh" "$range" ./) # linux"
-	echo
+	nr_commits_damon=$(git log --pretty=%h --no-merges "$range" \
+		-- $("$bindir/damon_source_files.py") | wc -l)
+	nr_commits_mm=$(git log --pretty=%h --no-merges "$range" -- mm/ | \
+		wc -l)
+	nr_commits_total=$(git log --pretty=%h --no-merges "$range" | wc -l)
 
+	line="$range	$nr_commits_damon	$nr_commits_mm	"
+	line+="$(awk "BEGIN {printf \"%.2f%%\", \
+		$nr_commits_damon * 100 / $nr_commits_mm}")"
+	line+="	$nr_commits_total	"
+	line+="$(awk "BEGIN {printf \"%.2f%%\", \
+		$nr_commits_damon * 100 / $nr_commits_total}")"
+	echo "$line"
 	prev_version=$version
 done
 range="${versions[0]}..${versions[-1]}"
-echo "# $range"
-echo "$("$bindir/_stat_damon_nr_commits_portion.sh" "$range" mm) # mm"
-echo "$("$bindir/_stat_damon_nr_commits_portion.sh" "$range" ./) # linux"
+nr_commits_damon=$(git log --pretty=%h --no-merges "$range" \
+	-- $("$bindir/damon_source_files.py") | wc -l)
+nr_commits_mm=$(git log --pretty=%h --no-merges "$range" -- mm/ | \
+	wc -l)
+nr_commits_total=$(git log --pretty=%h --no-merges "$range" | wc -l)
+
+line="$range	$nr_commits_damon	$nr_commits_mm	"
+line+="$(awk "BEGIN {printf \"%.2f%%\", \
+	$nr_commits_damon * 100 / $nr_commits_mm}")"
+line+="	$nr_commits_total	"
+line+="$(awk "BEGIN {printf \"%.2f%%\", \
+	$nr_commits_damon * 100 / $nr_commits_total}")"
+echo "$line"
