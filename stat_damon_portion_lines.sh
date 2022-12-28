8be74d13b2f80bd70b28620e8a1b7f7c9615456a
SeongJae Park <sj@kernel.org>
2022-12-28 01:23:15 +0000

    stat_damon_portion_lines: Update for better format
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- stat_damon_portion_lines.sh
+++ stat_damon_portion_lines.sh
@@ -10,18 +10,37 @@ bindir=$(realpath $(dirname "$0"))
 linux_dir=$1
 cd "$linux_dir"
 
+# echo "range	damon	mm	damon/mm	linux	damon/linux"
+echo "range	damon	mm	damon/mm"
 versions=( $(cat "$bindir/stat_branches") )
 prev_version="${versions[0]}"
 for version in "${versions[@]:1}"
 do
 	range="$prev_version..$version"
-	echo "# $range"
-	echo "$("$bindir/_stat_damon_lines_portion.sh" "$range" mm) # mm"
-	echo
+	nr_lines_damon=$("$bindir/_stat_lines.sh" "$range" mm/damon/ \
+		--no-merges | awk '{print $3}')
+	nr_lines_mm=$("$bindir/_stat_lines.sh" "$range" mm/ \
+		--no-merges | awk '{print $3}')
+	damon_per_mm=$(awk "BEGIN {printf \"%.2f%%\", \
+		$nr_lines_damon * 100 / $nr_lines_mm}")
+	line="$range	$nr_lines_damon	$nr_lines_mm	$damon_per_mm"
+#	line+="	skip	skip"
+	echo "$line"
 
 	prev_version=$version
 done
 range="${versions[0]}..${versions[-1]}"
-echo "# $range"
-echo "$("$bindir/_stat_damon_lines_portion.sh" "$range" mm) # mm"
-echo "$("$bindir/_stat_damon_lines_portion.sh" "$range" ./) # linux"
+nr_lines_damon=$("$bindir/_stat_lines.sh" "$range" mm/damon/ \
+	--no-merges | awk '{print $3}')
+nr_lines_mm=$("$bindir/_stat_lines.sh" "$range" mm/ \
+	--no-merges | awk '{print $3}')
+damon_per_mm=$(awk "BEGIN {printf \"%.2f%%\", \
+	$nr_lines_damon * 100 / $nr_lines_mm}")
+# nr_lines_linux=$("$bindir/_stat_lines.sh" "$range" ./ \
+# 	--no-merges | awk '{print $3}')
+# damon_per_linux=$(awk "BEGIN {printf \"%.2f%%\", \
+# 	$nr_lines_damon * 100 / $nr_lines_linux}")
+
+line="$range	$nr_lines_damon	$nr_lines_mm	$damon_per_mm"
+# line+="	$nr_lines_linux	$damon_per_linux"
+echo "$line"
