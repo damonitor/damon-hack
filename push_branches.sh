11d4cd47baef973c79098c79e02f6e4e0b0d94b8
SeongJae Park <sj@kernel.org>
2023-10-08 01:37:06 +0000

    push_branches: Push master branches
    Let push_branches to push master branches together.  It references the
    mainline, even for gh.damon.  In other words, damon/master branch is
    deprecated now.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- push_branches.sh
+++ push_branches.sh
@@ -6,9 +6,12 @@ bindir=$(dirname "$0")
 
 echo "sj.korg"
 git push sj.korg damon/next --force
+git push sj.korg linus/master:master
 
 echo "gh.public"
 git push gh.public damon/next --force
+git push gh.public linus/master:master
 
 echo "gh.damon"
 git push gh.damon damon/next:next --force
+git push gh.damon linus/master:master
