cb6b5d060819e3477a97268cb55fb534cdbcb2cc
SeongJae Park <sj@kernel.org>
2023-10-18 00:29:37 +0000

    push_branches: Fetch mainline before pushing
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- push_branches.sh
+++ push_branches.sh
@@ -4,6 +4,8 @@ echo "ensure gpg password"
 bindir=$(dirname "$0")
 "$bindir/ensure_gpg_password.sh"
 
+git fetch linus
+
 echo "sj.korg"
 git push sj.korg damon/next --force
 git push sj.korg linus/master:master
