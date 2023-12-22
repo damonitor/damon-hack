fa7b344db6303fa351cc65cefcc83ac393ec6c73
SeongJae Park <sj@kernel.org>
2023-12-22 13:00:31 -0800

    ensure_gpg_password: Add more checks
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- ensure_gpg_password.sh
+++ ensure_gpg_password.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+bindir=$(dirname "$0")
+
+if ! diff "$bindir/_ssh_config" $HOME/.ssh/config
+then
+	echo "Please set $HOME/.ssh/config same to $bindir/_ssh_config"
+	exit 1
+fi
+
+
+if ! grep --quiet "^export SSH_AUTH_SOCK=\$(gpgconf --list-dirs agent-ssh-socket)" "$HOME/.bashrc"
+then
+	echo "Add export SSH_AUTH_SOCK=\$(gpgconf --list-dirs agent-ssh-socket) to .bashrc"
+	exit 1
+fi
+
+if [ ! $(git config --get gpg.program) = "gpg" ]
+then
+	echo "do git config gpg.program gpg"
+	exit 1
+fi
+
 gpg-connect-agent updatestartuptty /bye > /dev/null
 ssh gitolite.kernel.org help > /dev/null
 ssh git@github.com &> /dev/null
