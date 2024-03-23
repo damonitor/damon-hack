3691800e21c1199999a6f263e83657b36703ecd6
SeongJae Park <sj@kernel.org>
2024-03-23 13:10:28 -0700

    Add a script for converting commits to patches for backup purpose
    Signed-off-by: SeongJae Park <sj@kernel.org>

---
+++ backup_patches.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]
+then
+	echo "Usage: $0 <directory> <commits>"
+	exit 1
+fi
+
+dest_dir=$1
+commits=$2
+
+mkdir -p "$dest_dir"
+if [ -e "$dest_dir/series" ]
+then
+	rm -f "$dest_dir/series"
+fi
+
+for commit in $(git log --reverse --pretty=%H "$commits")
+do
+	patch=$(git format-patch "$commit^".."$commit")
+	final_name=$(echo "$patch" | cut -c 6-)
+	mv "$patch" "$dest_dir/$final_name"
+	echo "$final_name" >> "$dest_dir/series"
+done
