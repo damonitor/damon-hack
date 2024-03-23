faaf8e78adda28f70e8a13f62a2870ed401988e5
SeongJae Park <sj@kernel.org>
2024-03-23 13:16:36 -0700

    backup_patches: Put baseline on series file
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- backup_patches.sh
+++ backup_patches.sh
@@ -17,6 +17,11 @@ fi
 
 for commit in $(git log --reverse --pretty=%H "$commits")
 do
+	if [ ! -e "$dest_dir/series" ]
+	then
+		baseline=$(git log --pretty=%H -1 "$commit^")
+		echo "$baseline" > "$dest_dir/series"
+	fi
 	patch=$(git format-patch "$commit^".."$commit")
 	final_name=$(echo "$patch" | cut -c 6-)
 	mv "$patch" "$dest_dir/$final_name"
