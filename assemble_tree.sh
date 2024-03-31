0dd38745a5a283865ad1626e3b919839b5cfa922
SeongJae Park <sj@kernel.org>
2024-03-31 12:50:52 -0700

    Add a script for assembling tree from patch series
    Signed-off-by: SeongJae Park <sj@kernel.org>

---
+++ assemble_tree.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <series file>"
+	exit 1
+fi
+
+series_file=$(realpath "$1")
+patches_dir=$(dirname "$series_file")
+
+baseline_checked_out="false"
+for patch in $(cat "$series_file")
+do
+	if [ "$baseline_checked_out" = "false" ]
+	then
+		git checkout "$patch"
+		baseline_checked_out="true"
+		continue
+	fi
+	git am "$patches_dir/$patch"
+done
