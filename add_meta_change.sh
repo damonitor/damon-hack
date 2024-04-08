bae5fa0ca2bfac8bf65c7e33aa506a3b3aebf541
SeongJae Park <sj@kernel.org>
2024-04-08 16:05:11 -0700

    Add a script for making meta change
    Signed-off-by: SeongJae Park <sj@kernel.org>

---
+++ add_meta_change.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Read damon_meta_changes/README for details.
+
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <meta change message>"
+	exit 1
+fi
+
+msg=$1
+
+mktemp damon_meta_changes/XXXXXXXX
+git add damon_meta_changes
+git commit -as -m "$msg"
