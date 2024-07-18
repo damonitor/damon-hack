4a82c94cdda57cf9aa17a88779523bdb35d9076a
SeongJae Park <sj@kernel.org>
2024-07-18 12:34:34 -0700

    scrits/rebase_damon_next_on_mm_unstable: Skip gpg ensuring
    The script does fetch mm tree, which is public.  No gpg tooling ensuring
    is needed.  Skip the task.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- scripts/rebase_damon_next_on_mm_unstable.sh
+++ scripts/rebase_damon_next_on_mm_unstable.sh
@@ -40,10 +40,6 @@ new_mm_unstable=akpm.korg.mm/mm-unstable
 
 bindir=$(dirname "$0")
 
-if ! "$bindir/ensure_gpg_password.sh"
-then
-	echo "ensure_gpg_password failed, as expected"
-fi
 git fetch akpm.korg.mm
 
 old_mm_unstable_commit=$(git rev-parse "$old_mm_unstable")
