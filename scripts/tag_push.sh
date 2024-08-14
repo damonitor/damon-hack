ca545e727b71821b6c08e9f0b02b17f8aaae906e
SeongJae Park <sj@kernel.org>
2024-08-14 11:52:33 -0700

    scripts: Add a script for tagging damon-next and push
    Signed-off-by: SeongJae Park <sj@kernel.org>

---
+++ scripts/tag_push.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+# Tag damon-hack with date and baseline, and push
+
+echo "ensure gpg password"
+bindir=$(dirname "$0")
+if ! "$bindir/ensure_gpg_password.sh"
+then
+	echo "ensure_gpg_password failed as expected"
+fi
+
+datetime=$(date +"%Y-%m-%d-%H-%M")
+baseline=$(git describe --match "v*" --abbrev=0)
+tagname=damon/next-$datetime-on-$baseline
+echo "tagging as $tagname"
+git -C "$bindir" tag -s "$tagname"
+
+echo "push"
+echo "gh"
+git -C "$bindir" push gh "$tagname"
+echo "korg"
+git -C "$bindir" push korg "$tagname"
