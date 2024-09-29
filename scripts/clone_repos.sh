80ca415b505d627852523d923fe522fbdc880873
SeongJae Park <sj@kernel.org>
2024-09-29 10:19:12 -0700

    scripts/clone_repos: clone masim, too
    damon-tests/corr requires masim to be there.  Clone it.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- scripts/clone_repos.sh
+++ scripts/clone_repos.sh
@@ -21,6 +21,10 @@ git -C hackermail remote update
 git clone --origin gh git@github.com:damonitor/damon-tests
 git -C damon-tests checkout gh/next -b next
 
+git clone --origin gh.upstream git@github.com:sjp38/masim
+git -C masim remote add gh.downstream git@github.com:damonitor/masim
+git -C masim remote update
+
 git clone --origin gh.damon git@github.com:damonitor/linux
 git -C linux remote add gh.public git@github.com:sjp38/linux
 git -C linux remote add sj.korg git@gitolite.kernel.org:pub/scm/linux/kernel/git/sj/linux
