5624f6bdc3565da5e01a6338f8fa8a73f12b51bb
SeongJae Park <sj@kernel.org>
2025-02-20 08:55:03 -0800

    scripts/patches_uptodate: check up to last 42 patches to get assembled tree hah
    The script assumes the last commit of damon-hack.git is the one for
    next patches queue update.  Sometimes it is for others, like scripts or
    posted patches queue.  Check up to last 42 patches.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- scripts/patches_uptodate.sh
+++ scripts/patches_uptodate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 bindir=$(dirname "$0")
-assembled_tree=$(git -C "$bindir" log -1 --pretty=%b | \
-	grep "^Assembled tree:" | awk '{print $3}')
+assembled_tree=$(git -C "$bindir" log -42 --pretty=%b | \
+	grep "^Assembled tree:" | head -n 1 | awk '{print $3}')
 linux_head="$(git rev-parse HEAD)"
 if [ "$assembled_tree" = "$linux_head" ]
 then
