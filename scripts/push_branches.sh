9e5bd05d76c753b3596a9a2e072f7a049dbb0ce9
SeongJae Park <sj@kernel.org>
2025-08-01 17:29:04 -0700

    scripts/push_branches: add refs/heads/
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- scripts/push_branches.sh
+++ scripts/push_branches.sh
@@ -11,23 +11,23 @@ git fetch linus
 echo "sj.korg"
 git push sj.korg damon/next --force
 git push sj.korg linus/master:master
-git push sj.korg akpm.korg.mm/mm-new:mm-new
-git push sj.korg akpm.korg.mm/mm-stable:mm-unstable
-git push sj.korg akpm.korg.mm/mm-stable:mm-stable
+git push sj.korg akpm.korg.mm/mm-new:refs/heads/mm-new
+git push sj.korg akpm.korg.mm/mm-stable:refs/heads/mm-unstable
+git push sj.korg akpm.korg.mm/mm-stable:refs/heads/mm-stable
 
 echo "gh.public"
 git push gh.public damon/next --force
 git push gh.public linus/master:master
-git push gh.public akpm.korg.mm/mm-new:mm-new
-git push gh.public akpm.korg.mm/mm-stable:mm-unstable
-git push gh.public akpm.korg.mm/mm-stable:mm-stable
+git push gh.public akpm.korg.mm/mm-new:refs/heds/mm-new
+git push gh.public akpm.korg.mm/mm-stable:refs/heads/mm-unstable
+git push gh.public akpm.korg.mm/mm-stable:refs/heads/mm-stable
 
 echo "gh.damon"
 git push gh.damon damon/next:next --force
 git push gh.damon linus/master:master
-git push gh.damon akpm.korg.mm/mm-new:mm-new
-git push gh.damon akpm.korg.mm/mm-stable:mm-unstable
-git push gh.damon akpm.korg.mm/mm-stable:mm-stable
+git push gh.damon akpm.korg.mm/mm-new:refs/heads/mm-new
+git push gh.damon akpm.korg.mm/mm-stable:refs/heads/mm-unstable
+git push gh.damon akpm.korg.mm/mm-stable:refs/heads/mm-stable
 
 echo
 "$bindir/push_damon_hack.sh"
