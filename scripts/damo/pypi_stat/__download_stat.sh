659f5c918175f4bfbc56855eae70c7be453d8f3b
SeongJae Park <sj@kernel.org>
2025-11-01 12:24:31 -0700

    scripts/damo/pypi_stat: use venv
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- scripts/damo/pypi_stat/__download_stat.sh
+++ scripts/damo/pypi_stat/__download_stat.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+set -e
+
 if [ $# -ne 1 ]
 then
 	echo "Usage: $0 <json file to save the stat>"
 	exit 1
 fi
 
+if [ ! -d pypistats_venv ]
+then
+	python3 -m venv pypistats_venv
+fi
+
+source ./pypistats_venv/bin/activate
+if ! which pypistats
+then
+	pip install pypistats
+fi
 pypistats overall damo --daily --mirrors without -f json > "$1"
+deactivate
