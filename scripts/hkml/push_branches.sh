887e41dde3ed21e0784cfc5ede603311a4c87a4a
SeongJae Park <sj@kernel.org>
2026-01-04 11:28:46 -0800

    scripts/{damo,hkml}/push_branches: check unsquashed fixups
    It is usual to commit fixups for local changes aiming to squash those
    fixups into the local changes before pushing.  But it is even more usual
    to forget the suqashing.  Check it from the script.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- scripts/hkml/push_branches.sh
+++ scripts/hkml/push_branches.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# it is usual to add fixup commits of local commits that aimed to be squashed
+# to the local commits before pushing.  But it is also usual forgetting the
+# squashing.  Check it.
+if git log --pretty="%s (%h)" korg/master..master | grep "^fixup "
+then
+	echo "seems the above fixup is not yet squashed"
+	exit 1
+fi
+
 echo "ensure gpg password"
 bindir=$(dirname "$0")
 "$bindir/../ensure_gpg_password.sh"
