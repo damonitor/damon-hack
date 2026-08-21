5b741f84e9322789053afec933466e0b6cca4bf3
SJ Park <sj@kernel.org>
2026-08-21 07:40:23 -0700

    scripts/release_news/mk_template_data: print DAMON contributors stat
    Signed-off-by: SJ Park <sj@kernel.org>

--- scripts/release_news/mk_template_data.py
+++ scripts/release_news/mk_template_data.py
@@ -17,12 +17,29 @@ def pr_damon_commits(linux_dir, base, tip, damon_src_files):
         exit(1)
     print(len(res.stdout.strip().split('\n')))
 
+def pr_contributors(base, tip, repo, lazybox_dir, is_linux):
+    authors_py = os.path.join(
+            lazybox_dir, 'version_control', 'authors.py')
+    cmd = [authors_py, repo, '--since', base, '--until', tip,
+           '--skip_merge_commits']
+    if is_linux:
+        cmd += ['--linux_subsystems', 'DAMON']
+    res = subprocess.run(cmd, capture_output=True, text=True)
+    print(' '.join(cmd))
+    if res.returncode != 0:
+        print('\ncmd failed\n')
+        print(res.stderr)
+        exit(1)
+    print(res.stdout)
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('linux_dir', help='path to the linux local repo')
     parser.add_argument(
             'linux_version_range', nargs=2,
             help='base and tip linux versions, e.g., v7.3-rc1 and v7.4-rc1')
+    parser.add_argument('damo_dir', help='path to the damo local repo')
+    parser.add_argument('lazybox_dir', help='path to the lazybox local repo')
 
     args = parser.parse_args()
 
@@ -43,6 +60,11 @@ def main():
     print()
     pr_damon_commits(args.linux_dir, last_major_release, linux_ver_tip,
                      src_files)
+
+    print()
+    print('Contributors')
+    pr_contributors(linux_ver_base, linux_ver_tip, args.linux_dir,
+                    args.lazybox_dir, is_linux=True)
 
 if __name__ == '__main__':
     main()
